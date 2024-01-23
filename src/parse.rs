64080344dadbc7ef8fa7cd04b5b2896ddefc27f5
Harry Sharma <harrysharma1066@gmail.com>
2024-01-23 15:24:57 +0000

    add: consume char function

--- src/parse.rs
+++ src/parse.rs
@@ -1,21 +1,29 @@
 
 struct Parser{
-    pos: usize,
+    position: usize,
     input: String,
 }
 
 impl Parser{
     fn next_char(&self)->char{
-        self.input[self.pos..].chars().next().unwrap()
+        self.input[self.position..].chars().next().unwrap()
     }
 
     fn starts_with(&self, check: &str)->bool{
-        self.input[self.pos..].starts_with(check)
+        self.input[self.position..].starts_with(check)
     } 
 
     fn eof(&self)->bool{
-        self.pos>self.input.len()
+        self.position>self.input.len()
     } 
+
+    fn consume_character(&mut self)->char{
+        let mut iterator = self.input[self.position..].char_indices();
+        let (_,current_character) = iterator.next().unwrap();
+        let (next_position,_) = iterator.next().unwrap_or((1,' '));
+        self.position+=next_position;
+        return current_character;
+    }
 
     
 }
